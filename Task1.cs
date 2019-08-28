1617e4ea7555a5cf322b882efc2dfd1237e7ba46
Vitaliy S <BYKET_1994@UKR.NET>
2019-08-28 18:11:14 +0300

    Project3

---
+++ Task1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        #region 1 завдання
+        Console.WriteLine("1 завдання");
+        Console.ReadKey();
+        List<Person> persons = new List<Person> {
+        new Person(){name="Roman",age=15,phoneNumbers = new List<string> {"+380999999999","+0353225560"}},
+        new Person(){name="Takia",age=51,phoneNumbers = new List<string> {"+3809913323239","+023251560"}},
+        new Person(){name="Momo",age=18,phoneNumbers = new List<string> {"+380911111199","+0222222220"}},
+        new Person(){name="Mazen",age=25,phoneNumbers = new List<string> {"+380933333399","+0354444440"}},
+        new Person(){name="Olga",age=35,phoneNumbers = new List<string> {"+380955555559","+0366666660"}},
+        new Person(){name="Vitalik",age=18,phoneNumbers = new List<string> {"+380977777999","+0377888760"}}
+        };
+        foreach (Person p in persons)
+        {
+            Console.WriteLine(p.name + " " + p.age);
+            Console.ReadKey();
+
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+        #endregion
+
+        #region 2 завдання
+        Console.WriteLine("2 завдання");
+        Console.ReadKey();
+        persons.AddRange(new List<Person>{
+        new Person(){name="Olga2",age=32,phoneNumbers = new List<string> {"+380955555524","+0366665510"}},
+        new Person(){name="Vitalik2",age=38,phoneNumbers = new List<string> {"+381111777999","+03772222760"}}
+        });
+        foreach (Person p in persons)
+        {
+            foreach (string phoneN in p.phoneNumbers)
+            {
+                Console.WriteLine(phoneN);
+                Console.ReadKey();
+            }
+        }
+        #endregion
+    }
+
+
+}
+
+public class Person
+{
+    public string name { get; set; }
+
+    public int age { get; set; }
+
+    public List<string> phoneNumbers { get; set; }
+
+}
---
+++ Task3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    const int initPageSize = 5;
+    public static void Main()
+    {
+        List<string> elements = new List<string> { };
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random random = new Random();
+        int size = 100;
+        while (elements.Count < size)
+        {
+            char[] stringChars = new char[4];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            elements.Add(new String(stringChars));
+        }
+        List<string> distinctElements = new List<string>(new HashSet<string>(elements));
+        List<string> filteredList = new List<string> { };
+        Console.WriteLine("Elements All");
+        
+        displayList(distinctElements);
+        foreach (string s in distinctElements)
+        {
+            if (!s.StartsWith("Z"))
+            {
+                filteredList.Add(s);
+            }
+
+        }
+        Console.WriteLine("Elements All");
+       
+        displayList(filteredList);
+        Console.WriteLine("Enter desired page:");
+        int desiredPage = int.Parse(Console.ReadLine());
+        Console.WriteLine("page " + desiredPage);
+        Console.ReadKey();
+        displayList(getPage(filteredList, desiredPage, initPageSize));
+
+
+    }
+
+    public static List<string> getPage(List<string> collection, int pageNumber, int pagesize)
+    {
+        int startElemntIndex = pageNumber * pagesize;
+        Console.WriteLine("startIndex " + startElemntIndex);
+       
+        int lastelemntIndex = startElemntIndex + pagesize;
+        Console.WriteLine("endIndex " + lastelemntIndex);
+       
+        Console.WriteLine("count " + collection.Count);
+        
+        if (lastelemntIndex > collection.Count) { pagesize = collection.Count % pagesize; }
+        if (startElemntIndex >= collection.Count) { Environment.Exit(1); return null; }
+        else
+            return collection.GetRange(startElemntIndex, pagesize);
+
+    }
+
+    public static void displayList(List<string> list)
+    {
+        foreach (string s in list)
+        {
+            Console.WriteLine(s);
+            
+
+
+        }
+    }
+}
